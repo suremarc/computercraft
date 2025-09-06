cd1c84ca1b06d830d24f2f53daa8dd50e6e3e0d9
suremarc <8771538+suremarc@users.noreply.github.com>
2025-09-06 13:11:34 -0500

    upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # computercraft
+
+## Dependencies
+
+* [DiscordHook](https://forums.computercraft.cc/index.php?topic=97.0)
---
+++ src/discord.lua
@@ -0,0 +1,8 @@
+local DiscordHook = require 'DiscordHook'
+
+local success, hook = Discord.createWebhook 'https://discord.com/api/webhooks/1413949188814667889/WvKZv5fWumxZQoIKWBMGZV2fEI1sfRYdlx90JTOVtKaG6nh1zSXo46BhOVqaApHphk6N'
+if not success then
+    error("Webhook connection failed (reason: " .. hook .. ")")
+end
+
+hook.send('this is a test message', nil, nil)
