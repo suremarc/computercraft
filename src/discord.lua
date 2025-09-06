19b3c802b4d3ac83f3eb1af319592a258ca6c0ee
suremarc <8771538+suremarc@users.noreply.github.com>
2025-09-06 13:13:10 -0500

    fix

--- src/discord.lua
+++ src/discord.lua
@@ -1,6 +1,6 @@
 local DiscordHook = require 'DiscordHook'
 
-local success, hook = Discord.createWebhook 'https://discord.com/api/webhooks/1413949188814667889/WvKZv5fWumxZQoIKWBMGZV2fEI1sfRYdlx90JTOVtKaG6nh1zSXo46BhOVqaApHphk6N'
+local success, hook = DiscordHook.createWebhook 'https://discord.com/api/webhooks/1413949188814667889/WvKZv5fWumxZQoIKWBMGZV2fEI1sfRYdlx90JTOVtKaG6nh1zSXo46BhOVqaApHphk6N'
 if not success then
     error("Webhook connection failed (reason: " .. hook .. ")")
 end
